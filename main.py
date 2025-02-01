6e55d5e5a845ba8f0a7a4ec0d7ff6e417f2cd9a3
shingyu <shyu@staredges.cn>
2025-02-01 19:33:14 +0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# 开发备注
+# 相同msg_id和msg_seq的消息会被去重，故最后回复的消息默认设置为了100
 import botpy
 from common import logger, ID, SECRET
 from handlers import commands_handler, static_handler
@@ -10,27 +12,27 @@ class MyClient(botpy.Client):
 
     @staticmethod
     async def on_group_at_message_create(message: GroupMessage):
-        content = message.content[1:] if message.content.startswith(" ") else message.content
+        content = message.content.lstrip(" ")
         logger.info(f"{message.group_openid}||{message.author.member_openid}||{content}")
         if content.startswith("/"):
             result = await commands_handler(message.author.member_openid, content, message)
-            await message.reply(content=result)
+            await message.reply(content=result, msg_seq=100)
         else:
             result = static_handler(content)
             if result:
-                await message.reply(content=result)
+                await message.reply(content=result, msg_seq=100)
 
     @staticmethod
     async def on_c2c_message_create(message: C2CMessage):
-        content = message.content[1:] if message.content.startswith(" ") else message.content
+        content = message.content.lstrip(" ")
         logger.info(f"C2C||{message.author.user_openid}||{content}")
         if content.startswith("/"):
             result = await commands_handler(message.author.user_openid, content, message, prefix="")
-            await message.reply(content=result)
+            await message.reply(content=result, msg_seq=100)
         else:
             result = static_handler(content, prefix="")
             if result:
-                await message.reply(content=result)
+                await message.reply(content=result, msg_seq=100)
 
 
 if __name__ == "__main__":
